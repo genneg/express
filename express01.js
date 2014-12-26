89d4700dea565f78a72f5872cb94560d27c435bd
genneg <genneg@yahoo.com>
2014-12-26 17:16:52 +0100

    Aggiunto Handlebars

--- express01.js
+++ express01.js
@@ -1,20 +1,29 @@
 var express= require('express');
 var app = express();
+
+var handlebars= require('express-handlebars').create({defaultLayout:'main'});
+app.engine('hbs', handlebars.engine);
+app.set('view engine', 'hbs');
+
 app.set('port', process.env.PORT || 3000);
+
 app.get('/', function(req, res){
-	res.type('text/plain');
-	res.send('CiaoCiao dalla app.get');
+	res.render('home');
 });
 app.get('/about', function(req,res){
-	res.type('text/plain');
-	res.send('Qwerty Siamo una piccola società che non lavora');
+	res.render('about');
 });
 app.use(function(req, res){
 	res.type('text/plain');
 	res.status(404);
-	res.send('404. Not Found');
+	res.render('404');
 });
-
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.type('text/plain');
+	res.status(500);
+	res.render('500');
+});
 app.listen(app.get('port'), function(){
-	console.log('Express stsrted on port '+app.get('port'));
+	console.log('Express started on port '+app.get('port'));
 });
